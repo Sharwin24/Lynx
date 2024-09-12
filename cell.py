2add7341fe0f92faeab84ea3c96c464e174d2d68
Sharwin24 <sharwin24@gmail.com>
2024-09-12 14:54:54 -0500

    docstrings and constructor for cell class

--- cell.py
+++ cell.py
@@ -1,9 +1,20 @@
 class Cell:
+    """ A class representing a cell within a maze. 
+    Contains information about the cell such as it's position (index within maze_list), wall or free cell, and neighbor cells
+    """
 
-    def __init__(self, index: int = None):
+    def __init__(self, index: int, neighbors: list[int] = [], is_wall: bool = True):
+        """ Creates a Cell with the given index. Defaults to a wall cell with empty neighbors
+
+        Args:
+            index (int): The position of the cell as an index within the maze_list. Defaults to None.
+            neighbors (list[int]): The neighboring cells that can be reached from this cell. Defaults to empty list
+            is_wall (bool): Denotes if this cell is a wall or not. Defaults to wall
+        """
         self.index = index
-        self.neighbors = []
-        self.is_wall = True
+        self.neighbors = neighbors
+        self.is_wall = is_wall
+        self.is_free = False
 
     def set_wall(self, is_wall: bool):
         self.is_wall = is_wall
@@ -22,3 +33,7 @@ class Cell:
 
     def __repr__(self) -> str:
         return f"{self.index}"
+
+    def __eq__(self, value: object) -> bool:
+        # Equality is based on the cell's index within maze_list
+        return self.index == value.index if isinstance(value, Cell) else False
