d15c5f9822738ec65797fcfd43f38962e8dcb995
Sharwin24 <sharwin24@gmail.com>
2024-09-12 16:08:47 -0500

    Solver has been solvedgit add solver.py

--- solver.py
+++ solver.py
@@ -1,12 +1,11 @@
 from cell import Cell
-from maze import *
+from maze import Maze, MazeInfo
 from collections import deque
 
-class Solver:
-    algo = any
-    maze = Maze()
 
-    def __init__(self, a : str, m : Maze):
+class Solver:
+
+    def __init__(self, a: str, m: Maze):
         self.algo = a
         self.maze = m
         self.path = []
@@ -16,7 +15,7 @@ class Solver:
         if self.algo == 0:
             self.wavefront(self.maze)
         elif self.algo == 1:
-            self.dfs(self.maze.maze_list[self.maze.start_index]) 
+            self.dfs(self.maze.maze_list[self.maze.start_index])
 
     def wavefront(self, m):
         weight = [0] * len(self.maze.maze_list)
@@ -27,8 +26,7 @@ class Solver:
         q.append(goal)
         self.visited.append(goal.get_index())
         while q:
-            front = q.pop(0)
-            print(front.get_index())
+            front = q.popleft()
             for n in front.get_neighbors():
                 if n not in self.visited:
                     weight[n] = weight[front.get_index()] + 1
@@ -36,8 +34,7 @@ class Solver:
                     self.visited.append(front.get_index())
 
         pos = self.maze.maze_list[self.maze.start_index]
-        print(weight)
-
+        self.path.append(pos.get_index())
         while pos.get_index() != goal.get_index():
             cur_weight = weight[pos.get_index()]
             next_move = -1
@@ -48,7 +45,7 @@ class Solver:
             self.path.append(next_move)
             pos = self.maze.maze_list[next_move]
 
-    def dfs(self, pos : Cell):
+    def dfs(self, pos: Cell):
         self.visited.append(pos.get_index())
         self.path.append(pos.get_index())
         if pos.get_index() == self.maze.goal_index:
@@ -63,29 +60,35 @@ class Solver:
             self.path.pop()
             return False
 
-        
+
 def main():
-    m = Maze([], MazeType.GridMaze, 0, 10, 0)
-    m.maze_list.append(Cell(0, [1], False))
-    m.maze_list.append(Cell(1, [0, 2, 3], False))
-    m.maze_list.append(Cell(2, [1], False))
-    m.maze_list.append(Cell(3, [1, 4], False))
-    m.maze_list.append(Cell(4, [5, 9, 3], False))
-    m.maze_list.append(Cell(5, [4, 6, 8], False))
-    m.maze_list.append(Cell(6, [5, 7], False))
-    m.maze_list.append(Cell(7, [6, 8], False))
-    m.maze_list.append(Cell(8, [5, 7], False))
-    m.maze_list.append(Cell(9, [4, 10], False))
-    m.maze_list.append(Cell(10, [9], False))
+    maze_list = []
+    maze_list.append(Cell(0, [1], False))
+    maze_list.append(Cell(1, [0, 2, 3], False))
+    maze_list.append(Cell(2, [1], False))
+    maze_list.append(Cell(3, [1, 4], False))
+    maze_list.append(Cell(4, [5, 9, 3], False))
+    maze_list.append(Cell(5, [4, 6, 8], False))
+    maze_list.append(Cell(6, [5, 7], False))
+    maze_list.append(Cell(7, [6, 8], False))
+    maze_list.append(Cell(8, [5, 7], False))
+    maze_list.append(Cell(9, [4, 10], False))
+    maze_list.append(Cell(10, [9], False))
+    m = Maze(info=MazeInfo(type=MazeInfo.MazeType.GridMaze, size=(4, 4)),
+             maze_list=maze_list,
+             start_index=0,
+             goal_index=10,
+             robot_index=0
+             )
 
     s = Solver(1, m)
     s.solve()
-    print(s.path)
+    print(f"DFS Path: {s.path}")
 
     w = Solver(0, m)
     w.solve()
-    print(w.path)
+    print(f"BFS Path: {w.path}")
 
 
-if __name__=="__main__":
-    main()  
+if __name__ == "__main__":
+    main()
