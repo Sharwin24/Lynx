447046de820aa0eda0bc1d96787f82dfe9e66b42
Sharwin24 <sharwin24@gmail.com>
2024-09-12 12:03:08 -0500

    Created files and wrote Generator class template

---
+++ generator.py
@@ -0,0 +1,10 @@
+from maze import Maze
+
+class Generator():
+  """Class that generates the the maze utilizing randomized prim's algorithm"""
+
+  def __init__(self) -> None:
+    pass
+
+  def generate_from_txt(self, txt_maze: str) -> Maze:
+    pass
