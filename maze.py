abb26d4e6f9591dfe7ec346f49cf5f2257a46891
Sharwin24 <sharwin24@gmail.com>
2024-09-12 14:29:25 -0500

    Generator functionality for finding index from row,column position

--- maze.py
+++ maze.py
@@ -13,14 +13,13 @@ class Maze():
     """ A class representing the entire maze, containing the list of cell objects, the type of maze, and the start/goal cells
     """
 
-    def __init__(self, maze_list: list[Cell] = None, maze_type: MazeType = MazeType.GridMaze, start_index: int = None, goal_index: int = None, robot_index: int = None) -> None:
+    def __init__(self, maze_list: list[Cell] = [], maze_type: MazeType = MazeType.GridMaze, start_index: int = None, goal_index: int = None, robot_index: int = None) -> None:
         """ Creates a maze object with optional arguments to create an object with initialized values.
             Default behavior will create an empty maze which will need the fields to be populated by using the Generator.
             See generator.py for the logic behind populating these fields.
-
             The two options for generating a maze are:
-                1. Generating the maze with generator and populating the Maze using the Maze.__init__() function with all the arguments
-                2. Creating an empty maze using Maze.__init()__ without any arguments, then generating a maze with the generator and populating the maze using Maze.populate_maze()
+            1. Generating the maze with generator and populating the Maze using the Maze.__init__() function with all the arguments
+            2. Creating an empty maze using Maze.__init()__ without any arguments, then generating a maze with the generator and populating the maze using Maze.populate_maze()
 
         Args:
             maze_list (list[Cell], optional): The maze represented as a list of Cell objects. Defaults to None.
@@ -29,7 +28,7 @@ class Maze():
             goal_index (int, optional): The goal cell represented as an index within the maze_list. Defaults to None.
             robot_index (int, optional): The cell the robot occupies as an index within the maze_list. Defaults to None.
         """
-        self.maze_list = [] if maze_list == None else self.maze_list = maze_list
+        self.maze_list = maze_list
         self.maze_type = maze_type
         self.start_index = start_index
         self.goal_index = goal_index
@@ -45,7 +44,11 @@ class Maze():
             goal_index (int): The goal cell represented as an index within the maze_list
             robot_index (int): The cell the robot occupies as an index within the maze_list
         """
-        pass
+        self.maze_list = maze_list
+        self.maze_type = maze_type
+        self.start_index = start_index
+        self.goal_index = goal_index
+        self.robot_index = robot_index
 
     def __repr__(self) -> str:
         """ Returns the string representation of the maze
