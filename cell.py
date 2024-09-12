1101c738b5886302cbf6bdb6b211b3f117859898
Andrew Kwolek <andrewkwolek2025@u.northwestern.edu>
2024-09-12 13:02:05 -0500

    Added cell class.

---
+++ cell.py
@@ -0,0 +1,25 @@
+class Cell:
+    index = 0
+    neighbors = []
+    is_wall = False
+
+    def __init__(self, i, n, w):
+        self.index = i
+        self.neighbors = n
+        self.is_wall = w
+
+    def set_wall(self, w):
+        self.is_wall = w
+
+    def set_free(self, f):
+        self.is_free = f
+
+    def get_neighbors(self):
+        return self.neighbors
+    
+    def get_wall(self):
+        return self.is_wall
+    
+    def get_index(self):
+        return self.index
+    
