27e1fdba833d89d2927fe934ca18577267dfd7d4
Andrew Kwolek <andrewkwolek2025@u.northwestern.edu>
2024-09-15 10:53:14 -0500

    Fixed BFS

--- solver.py
+++ solver.py
@@ -2,6 +2,7 @@ from cell import Cell
 from maze import Maze, MazeInfo
 from collections import deque
 from maze_interpreter import MazeInterpreter
+import numpy as np
 
 
 class Solver:
@@ -14,43 +15,48 @@ class Solver:
 
     def solve(self):
         if self.algo == 0:
-            self.wavefront(self.maze)
+            self.wavefront()
         elif self.algo == 1:
             self.dfs(self.maze.maze_list[self.maze.start_index])
 
-    def wavefront(self, m):
+    def wavefront(self):
         weight = [0] * len(self.maze.maze_list)
         goal = self.maze.maze_list[self.maze.goal_index]
-        print(goal.get_index())
         weight[goal.get_index()] = 2
+        print(self.visited)
+        # Assign walls
+        for c in self.maze.maze_list:
+            if c.get_wall():
+                weight[c.get_index()] = 1
+
+        w_vis = np.array(weight).reshape(self.maze.info.size)
+        print(f"Planner:\n {w_vis}")
 
         q = deque()
-
         q.append(goal)
         self.visited.append(goal.get_index())
         while q:
             front = q.popleft()
+            #print(front.get_index())
             for n in front.get_neighbors():
-                if n not in self.visited:
-                    if self.maze.maze_list[n].get_wall():
-                        weight[n] = 1
-                    else:
-                        weight[n] = weight[front.get_index()] + 1
+                if not (n in self.visited) and not self.maze.maze_list[n].get_wall():
+                    weight[n] = weight[front.get_index()] + 1
+                    self.visited.append(n)
                     q.append(self.maze.maze_list[n])
-            self.visited.append(front.get_index())
+            w_vis = np.array(weight).reshape(self.maze.info.size)
+            #print(f"Planner:\n {w_vis}")
 
-        pos = self.maze.maze_list[self.maze.start_index]
-        self.path.append(pos.get_index())
-        while pos.get_index() != goal.get_index():
-            print("Second")
-            cur_weight = weight[pos.get_index()]
+        pos = self.maze.start_index
+        self.path.append(pos)
+        while pos != goal.get_index():
+            cur_weight = weight[pos]
             next_move = -1
-            for n in pos.get_neighbors():
+            for n in self.maze.maze_list[pos].get_neighbors():
                 if weight[n] > 1 and weight[n] < cur_weight:
                     cur_weight = weight[n]
                     next_move = n
             self.path.append(next_move)
-            pos = self.maze.maze_list[next_move]
+            pos = self.maze.maze_list[next_move].get_index()
 
     def dfs(self, pos: Cell):
         self.visited.append(pos.get_index())
@@ -73,25 +79,7 @@ def main():
     filepath = "sample_maze_3.txt"
     mi = MazeInterpreter()
     loaded_maze = mi.interpret_external(filepath)
-
-    # maze_list = []
-    # maze_list.append(Cell(0, [1], False))
-    # maze_list.append(Cell(1, [0, 2, 3], False))
-    # maze_list.append(Cell(2, [1], False))
-    # maze_list.append(Cell(3, [1, 4], False))
-    # maze_list.append(Cell(4, [5, 9, 3], False))
-    # maze_list.append(Cell(5, [4, 6, 8], False))
-    # maze_list.append(Cell(6, [5, 7], False))
-    # maze_list.append(Cell(7, [6, 8], False))
-    # maze_list.append(Cell(8, [5, 7], False))
-    # maze_list.append(Cell(9, [4, 10], False))
-    # maze_list.append(Cell(10, [9], False))
-    # m = Maze(info=MazeInfo(type=MazeInfo.MazeType.GridMaze, size=(4, 4)),
-    #          maze_list=maze_list,
-    #          start_index=0,
-    #          goal_index=10,
-    #          robot_index=0
-    #          )
+    print(loaded_maze)
 
     s = Solver(1, loaded_maze)
     s.solve()
@@ -101,7 +89,6 @@ def main():
     w.solve()
     print(f"BFS Path: {w.path}")
 
-    print(loaded_maze)
 
 
 if __name__ == "__main__":
